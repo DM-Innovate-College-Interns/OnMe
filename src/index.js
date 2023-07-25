1bc6955505924e430db4fe41f15d8d3c20f5f4c4
Vaibhav Shrivathsa <Vaibhav.Shrivathsa@gmail.com>
2023-07-25 01:33:56 -0500

    Added Red/Green/Grey Amount Coloring to Group Select Screen Also improved responsive design on group select screen

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./Stylesheets/LoginScreen.css";
-import "./Stylesheets/General.css";
 
 import LoginScreen from "./Components/LoginScreen";
 import GroupSelectScreen from "./Components/GroupSelectScreen";
